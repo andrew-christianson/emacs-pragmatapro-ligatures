4294e29612ecd0e599cb8c3ac51a83fae1f51d6e
Andrew Christianson <achristianson@edmunds.com>
2020-11-20 09:14:39 -0800

    pkg: package header tp satisfy quelpa

--- pragmatapro-lig.el
+++ pragmatapro-lig.el
@@ -1,10 +1,36 @@
-;; Emacs PramgataPro 0.828 Ligatures Support
-;; Author: lumiknit (aasr4r4@gmail.com)
-;; Version: 20200211
+;;; pragmatapro-lig.el --- summary -*- lexical-binding: t -*-
 
+;; Author: lumiknit (aasr4r4@gmail.com)
+;; Maintainer: Andrew Christianson
+;; Version: 20200211
+;; Package-Requires: (dependencies)
+;; Homepage: https://github.com/andrew-christianson/emacs-pragmatapro-ligatures
+;; Keywords: keywords
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Emacs PramgataPro 0.828 Ligatures Support
 ;; Usage: Use "M-x 'pragmatapro-lig-mode' RET" to turn on ligature minor mode.
 ;;        Or, use 'pragmatapro-lig-global-mode to turn it on globally.
 ;;        I recommend you to compile this file before load it.
+
+;;; Code:
 
 (eval-when-compile (defconst pragmatapro-lig-alist
   '(("[ERROR]" #Xe2c0)
@@ -445,3 +471,5 @@
     (if z z mode-name)))
 
 (provide 'pragmatapro-lig)
+
+;;; pragmatapro-lig.el ends here
